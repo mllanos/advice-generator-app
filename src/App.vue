d0fcae2252595341c038f679809e5e33b7722451
mllanos <mario.llanos@appdirect.com>
2022-02-22 20:54:34 -0300

    Moving stuff.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Advice #<!-- Advice ID goes here -->
+
+  "<!-- Advice text goes here -->"
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.attribution {
+  font-size: 11px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.attribution a {
+  color: hsl(228, 45%, 44%);
 }
 </style>
